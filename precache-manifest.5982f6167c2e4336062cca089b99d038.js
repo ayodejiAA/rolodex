53051c580791197b9a346dfa393bc0633637ef4c
ayodejiAA <afolabiayodejia@gmail.com>
2019-10-06 02:51:15 +0100

    Updates

---
+++ precache-manifest.5982f6167c2e4336062cca089b99d038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e361477eb11d94dd6c0a0382f4c04d",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "4b0b62cd39b7ae08b42c",
+    "url": "/rolodex/static/css/main.bf115e87.chunk.css"
+  },
+  {
+    "revision": "cf92d4a2ea7f5a90bec1",
+    "url": "/rolodex/static/js/2.2f78e4ac.chunk.js"
+  },
+  {
+    "revision": "4b0b62cd39b7ae08b42c",
+    "url": "/rolodex/static/js/main.d39f0c42.chunk.js"
+  },
+  {
+    "revision": "e1af7f0f24caee8b5988",
+    "url": "/rolodex/static/js/runtime-main.1d0fa70f.js"
+  }
+]);
